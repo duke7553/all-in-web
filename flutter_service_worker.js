bccb4c72764e189310dde53c90d46c86f88ff849
Luke Blevins <lukeblevins15@outlook.com>
2020-10-21 18:15:06 -0400

    Sync with latest fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,21 +3,23 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "285422249f586265588167cecb579e83",
-"version.json": "b65dd64cc6487940ffc4ae58b07739cc",
+  "assets/AssetManifest.json": "5d87718f9c8b7ee63fa63868bb570fa6",
+"assets/FontManifest.json": "373f0459fc5ef9490d0d13f3353d2e50",
+"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+"assets/NOTICES": "96c12eb619e9c0bf63935a86da41d7e3",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.otf": "132a5e63b5e510933ab4845577716106",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/NOTICES": "c01c1c5d4e7350504044e27604d841c1",
+"assets/packages/fluentui_system_icons/fonts/FluentSystemIcons-Filled.ttf": "335eb57ea0638cc25f0147e924341772",
+"assets/packages/fluentui_system_icons/fonts/FluentSystemIcons-Regular.ttf": "5ff26b9eb48b0a0d8d6a1df1ffb85ad1",
 "favicon.png": "f03e898aa32662847536df9ba4b5f280",
 "icons/Icon-192.png": "2e32921ca6d5cf1fca838e77f8de1dd2",
 "icons/Icon-200.png": "d695a350c6c54fef81f114c02220251c",
 "icons/Icon-256.png": "511bfd00112dda420710358c164d1091",
 "icons/Icon-512.png": "0626ccb5bade071c8b64e2811d43b5e4",
-"index.html": "9183ac27d16deda1e506a677361e7793",
-"/": "9183ac27d16deda1e506a677361e7793",
-"manifest.json": "65ec571d89e9089bdc2f8de2bf72ac9d"
+"index.html": "2cbb28a820652db81c5c6f6a05ecfb19",
+"/": "2cbb28a820652db81c5c6f6a05ecfb19",
+"main.dart.js": "fa3c677f0714e1f368688260dbf93c47",
+"manifest.json": "d3ada8acae162e7d5d063e01d9287b7f",
+"version.json": "b65dd64cc6487940ffc4ae58b07739cc"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -36,7 +38,7 @@ self.addEventListener("install", (event) => {
     caches.open(TEMP).then((cache) => {
       cache.addAll(
         CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
-      return cache;
+        return cache;
     })
   );
 });
